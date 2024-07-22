68d82d80caf4441e33f8815a42c50990f7cd73fa
song <song7868@gmail.com>
2024-07-22 21:20:37 +0900

    ADD : signup views.py

--- ai_assistant/schedule/views.py
+++ ai_assistant/schedule/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            try:
+                user = form.save()
+                user.save()
+                login(request, user)
+                return redirect('home')
+            except Exception as e:
+                form.add_error(None, f"에러발생: {e}")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'signup.html', {'form': form})
