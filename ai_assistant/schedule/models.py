fd68f1c58af75366396441daf4a9146a24cbc8ef
song <song7868@gmail.com>
2024-07-27 17:50:32 +0900

    model change

--- ai_assistant/schedule/models.py
+++ ai_assistant/schedule/models.py
@@ -7,3 +7,21 @@ class CustomUser(AbstractUser):
     
     def __str__(self):
         return self.username
+    
+# 스케줄에 태그 붙이는 기능
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+# 스케줄
+class Schedule(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
+    
+    def __str__(self):
+        return self.title
