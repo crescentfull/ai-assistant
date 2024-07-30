af7c79d822e6cc9c7c977395befed1c4488695ba
song <song7868@gmail.com>
2024-07-31 00:41:11 +0900

    FIX : login method error

--- ai_assistant/schedule/forms.py
+++ ai_assistant/schedule/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser
+from .models import CustomUser, Schedule
 from allauth.account.forms import LoginForm
 
 class CustomUserCreationForm(UserCreationForm):
     student_id = forms.CharField(max_length=20, required=True, help_text='학번을 입력하세요')
     is_bt_student = forms.BooleanField(required=True, help_text='방통대학생이라면 체크하세요')
-
+    
     class Meta:
         model = CustomUser
         fields = ('username', 'student_id', 'is_bt_student', 'email', 'password1', 'password2')
