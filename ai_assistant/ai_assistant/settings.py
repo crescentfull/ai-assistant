c3fe443eac21008f2b0ddeadb805a84290c44034
song <song7868@gmail.com>
2024-08-22 21:20:31 +0900

    ADD : home google social login app 추가

--- ai_assistant/ai_assistant/settings.py
+++ ai_assistant/ai_assistant/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = "ai_assistant.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR), 'schedule','templates'],
+        "DIRS" : [BASE_DIR , 'schedule/templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
